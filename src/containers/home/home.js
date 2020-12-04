f20a0e247770e95b26cc07db4508c87ebb34bb10
FisherKai <31927262+FisherKai@users.noreply.github.com>
2020-12-04 14:40:20 +0800

    update

--- src/containers/home/home.js
+++ src/containers/home/home.js
@@ -1,10 +1,8 @@
 import { PureComponent } from 'react';
 import Live2D from '../../components/Live2D/index';
-import { Menu, Switch, Avatar, Image } from 'antd';
+import { Menu, Image } from 'antd';
 
 const imageUrl = require('../../asset/image/Avatar.jpg');
-console.log('imageUrl', imageUrl)
-const { SubMenu } = Menu;
 
 class Home extends PureComponent {
     state = {
@@ -13,12 +11,6 @@ class Home extends PureComponent {
 
     handleClick = e => {
         console.log('click ', e);
-    };
-
-    changeTheme = value => {
-        this.setState({
-            theme: value ? 'dark' : 'light',
-        });
     };
 
     render() {
@@ -31,40 +23,16 @@ class Home extends PureComponent {
                     mode="inline"
                     theme={this.state.theme}
                 >
-                    <Avatar className="home-u-avatar"
-                        src={<Image src={imageUrl.default} />}
-                    />
-                    <SubMenu key="sub1" title="Navigation One">
-                        <Menu.ItemGroup key="g1" title="Item 1">
-                            <Menu.Item key="1">Option 1</Menu.Item>
-                            <Menu.Item key="2">Option 2</Menu.Item>
-                        </Menu.ItemGroup>
-                        <Menu.ItemGroup key="g2" title="Item 2">
-                            <Menu.Item key="3">Option 3</Menu.Item>
-                            <Menu.Item key="4">Option 4</Menu.Item>
-                        </Menu.ItemGroup>
-                    </SubMenu>
-                    <SubMenu key="sub2" title="Navigation Two">
-                        <Menu.Item key="5">Option 5</Menu.Item>
-                        <Menu.Item key="6">Option 6</Menu.Item>
-                        <SubMenu key="sub3" title="Submenu">
-                            <Menu.Item key="7">Option 7</Menu.Item>
-                            <Menu.Item key="8">Option 8</Menu.Item>
-                        </SubMenu>
-                    </SubMenu>
-                    <SubMenu key="sub4" title="Navigation Three">
-                        <Menu.Item key="9">Option 9</Menu.Item>
-                        <Menu.Item key="10">Option 10</Menu.Item>
-                        <Menu.Item key="11">Option 11</Menu.Item>
-                        <Menu.Item key="12">Option 12</Menu.Item>
-                    </SubMenu>
-                    <Switch
-                        className="home-meun-change-theme"
-                        checked={this.state.theme === 'dark'}
-                        onChange={this.changeTheme}
-                        checkedChildren="Dark"
-                        unCheckedChildren="Light"
-                    />
+                    <Image src={imageUrl.default} className="home-u-avatar" />
+                    <Menu.Item key="sub1">
+                        个人信息
+                    </Menu.Item>
+                    <Menu.Item key="sub2">
+                        个人项目演示
+                    </Menu.Item>
+                    <Menu.Item key="sub3">
+                        有趣的东西
+                    </Menu.Item>
                 </Menu>
                 {/* Live2D看板娘 */}
                 <Live2D></Live2D>
