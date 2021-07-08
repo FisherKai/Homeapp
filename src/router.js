cad4789a7a607395f27d367c92f2380258922cd8
yukai pc <1079869931@qq.com>
2021-07-09 00:25:15 +0800

    update

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
-import HomePage from './containers/home/home';
-import facedemo from './containers/facedemo/facedemo';
+import HomePage from './pages/home/home';
+import facedemo from './pages/facedemo/facedemo';
 
 
 function RouterConfig({ history }) {
